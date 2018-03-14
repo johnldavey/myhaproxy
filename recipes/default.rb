58bbb0444add3b4e20719967e348489228f5e03b
johnldavey <johnldavey@me.com>
2018-03-14 11:06:11 -0400

    version 2.0

--- recipes/default.rb
+++ recipes/default.rb
@@ -6,9 +6,15 @@
 
 node.default['haproxy']['members'] = [{
     'hostname' => 'ec2-54-159-97-4.compute-1.amazonaws.com',
-    'ipaddress' => '172.31.51.34',
+    'ipaddress' => '54.159.97.4',
     'port' => 80,
     'ssl_port' => 80
-  }]
+  },{
+    'hostname' => 'ec2-54-210-123-139.compute-1.amazonaws.com',
+    'ipaddress' => '54.210.123.139',
+    'port' => 80,
+    'ssl_port' => 80
+  }
+]
 
 include_recipe 'haproxy::manual'
